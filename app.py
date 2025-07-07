80d13ab531442fbef14382d578b9df8a7ff07f6b
keejkrej <ctyjackcao@outlook.com>
2025-07-07 13:13:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from peak_extract.controller import run_app
 # Constants
 DATA_ROOT = Path("/project/ag-nickel/Strahlzeiten/DESY_P62_June_2025/raw/")
 EXPERIMENT = "X3_shower1"
-FOLDER_DATA = DATA_ROOT / EXPERIMENT
+FOLDER_DATA = DATA_ROOT / EXPERIMENT / "saxs"
 FILE_DATA = FOLDER_DATA / f"{EXPERIMENT}_00001_master.h5"
 SAVE_ROOT = Path("/home/t/Tianyi.Cao/scratch/shower/")
 FOLDER_RESULT = SAVE_ROOT / EXPERIMENT
